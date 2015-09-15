5538947c07d1bdfecef83632233900eb8ba740d2
Petr Mifek <petr.mifek@anapol.cz>
2015-09-15 18:43:47 +0200

    Carthage compatibility (Mac + some parts for iOS)

---
+++ F53OSC Framework/F53OSC/F53OSC (Mac)/F53OSC.h
@@ -0,0 +1,22 @@
+
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for F53OSC (Mac).
+FOUNDATION_EXPORT double F53OSCVersionNumber;
+
+//! Project version string for F53OSC (Mac).
+FOUNDATION_EXPORT const unsigned char F53OSCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F53OSC/PublicHeader.h>
+
+
+#import <F53OSC/F53OSCProtocols.h>
+#import <F53OSC/F53OSCParser.h>
+#import <F53OSC/F53OSCSocket.h>
+#import <F53OSC/F53OSCPacket.h>
+#import <F53OSC/F53OSCMessage.h>
+#import <F53OSC/F53OSCBundle.h>
+#import <F53OSC/F53OSCClient.h>
+#import <F53OSC/F53OSCServer.h>
+#import <F53OSC/F53OSCTimeTag.h>
---
+++ F53OSC Framework/F53OSC/F53OSC (iOS)/F53OSC (iOS).h
@@ -0,0 +1,19 @@
+//
+//  F53OSC (iOS).h
+//  F53OSC (iOS)
+//
+//  Created by Petr Mifek on 15.09.15.
+//  Copyright (c) 2015 Figure 53. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for F53OSC (iOS).
+FOUNDATION_EXPORT double F53OSC__iOS_VersionNumber;
+
+//! Project version string for F53OSC (iOS).
+FOUNDATION_EXPORT const unsigned char F53OSC__iOS_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F53OSC__iOS_/PublicHeader.h>
+
+
