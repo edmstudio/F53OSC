9b78ef202fd11f48ad2ad05b0e73c4e481476776
Petr Mifek <petr.mifek@anapol.cz>
2016-04-15 00:31:25 +0200

    Retain the dispatch queue again...

--- F53OSCSocket.m
+++ F53OSCSocket.m
@@ -43,7 +43,7 @@
 @interface F53OSCStats ()
 
 @property (strong) NSDate *currentTime;
-@property (assign) dispatch_queue_t timerQueue;
+@property (retain) dispatch_queue_t timerQueue;
 @property (assign) bool stopCounting;
 
 @property (assign) double totalBytes;
